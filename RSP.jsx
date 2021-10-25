89e36a72d5fda2eaaabb6e2a2474081ed0c4831d
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-10-25 23:48:14 +0900

    1025 class -> hook 변환

--- RSP.jsx
+++ RSP.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 
 // 클래시의 경우 -> constructor  -> render -> ref -> componentDidMount
 // -> (setState/props 바뀔 때 -> shouldComponentUpdate -> render -> componentDidUpdate)
@@ -37,6 +37,39 @@ const RSP = () => {
             setImgCoord(rspCoords.바위)
         }
     };
+
+    const onClickBtn = (choice) => {
+        const { imgCoord } = this.state;
+        clearInterval(this.interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+            setResult('비겼습니다!');
+        } else if ([-1, 2].includes(diff)) {
+            setResult('이겼습니다!');
+            setScore((prevScore) => prevScore + 1);
+        } else {
+            setResult('졌습니다!');
+            setScore((prevScore) => prevScore - 1);
+        }
+        setTimeout(() => {
+            interval.current = setInterval(changeHandle, 500);
+        }, 2000);
+    };
+
+    return(
+        <>
+            <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}} />
+            <div>
+                <button id="rock" className="btn" onClick={() => { onClickBtn('바위')}}>바위</button>
+                <button id="scissors" className="btn" onClick={() => { onClickBtn('가위')}}>가위</button>
+                <button id="paper" className="btn" onClick={() => { onClickBtn('보')}}>보</button>
+            </div>
+            <div>{result}</div>
+            <div>현재 {score}점</div>
+        </>
+    );
 }
 
 class RSP extends Component {
